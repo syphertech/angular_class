1d6c52912007dfafe980001935736c3d7e5c4c71
maxwell kobe <mxncoby@gmail.com>
2017-03-25 11:05:32 -0400

    r

---
+++ Script.js
@@ -0,0 +1,10 @@
+///reference path="angular.min.js"/>
+var myApp= angular.module("myModule",[]);
+
+
+
+
+myApp.controller("myController", function($scope)
+{
+  $scope.message="Angular JS Tutorial";
+});
--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 (function()
 {
-'use strict';
+  'use strict';
 
- angular.module('myFirstApp',[])
+ angular.module('DIApp',[])
+.controller('DIController',DIController);
 
-
+function DIController($scope,$filter){
+$scope.name= "Maxwell";
+//$scope.age="34";
+//$scope.sayHello= function(){
+//  return "hello you are my friend coursera";}
+//
+$scope.upper=function(){
+  var upCase=$filter('uppercase');
+  $scope.name=upCase($scope.name);
+};
+}
+function AnnotateMe(name ,job,blah){
+  return "blah";
+}
+console.log(AnnotateMe.toString());
 })();
